57684498abf8b545534fc740af3a3ae273673a14
Maybell Obadoni <35500803+frontEndDoctor@users.noreply.github.com>
2022-04-09 13:15:33 +0100

    Updated

--- demo.js
+++ demo.js
@@ -1,57 +1,57 @@
+// This is a script for tutorial/demo purposes on JS 
+// To run code independently, uncomment the block and run
 
-// // Difference between var,let,const
-// // Data types(Number(float, int), String, Boolean,Null, Undefined, Object)
-// // Checking the data type(typeof)
-// // concatenation
-// // Checking the data type after concatenating
-// // Datatype conversion(implicit,explicit)
+//Topic 1: Difference between var,let,const
+// Data types(Number(float, int), String, Boolean,Null, Undefined, Object)
+// Checking the data type(typeof)
+// concatenation
+// Checking the data type after concatenating
+// Datatype conversion(implicit,explicit)
 
-// // var x = "hello";
-// // var y = "world";
-// // var z = x + " " +y;
-// // console.log(z);
-// // var a = '13';
-// // var b = '17';
-// // var c = true;
-// // var z = a+b+c;
-// // console.log(typeof z);
+// Examples for Day 1:
+var x = "hello";
+var y = "world";
+var z = x + " " +y;
+console.log(z);
 
 
-// // // var doctor = 'Ben';
-// // var age =String(45);
-// // var height = Number("m" + 30.16);
-// // var doctor = {
-// //     n :'Ben',
-// //     age: 45,
-// //     height:"m" + 30.16;
-// // }
+var a = '13';
+var b = '17';
+var c = true;
+var u = a+b+c;
+console.log(typeof u);
 
+// More examples on data types
+var doctor = 'Ben';
+var age =String(45);
+var height = Number("m" + 30.16);
 
-// // console.log(typeof doctor);
-// // console.log(age);
-// // console.log(typeof height);
-
-// // console.log(age);
-
-
-
-
-
+// Example of Object
+var doctor = {
+n :'Ben',
+age: 45,
+height:"m" + 30.16
+}
+console.log(typeof doctor);
+console.log(age);
+console.log(typeof height);
 
 
 
 
 
+// Topic 2: Variable Scope and keywords
+// var keyword allows updating and re-declaring both globally and locally
+// const keyword does no allow redeclaring or updating
+// let keyword allows redeclaring and updating without using  "let" 
 
 
+var x = "Chisom";
+function A(){
+    console.log(x);
+var x = "Zoba";
+};
 
-
-// // var x = "Chisom";
-// // function A(){
-// //     console.log(x);
-// //     // var x = "Zoba";
-
-// // };
 // // var x = "Zoba";
 // // A();
 // // let x = "Chisom";
@@ -82,25 +82,24 @@
 // // console.log(height);
 
 
-// // program to convert date to number
-// // create date
-// // const d1 = new Date();
-// // console.log(d1);
+//program to convert date to number
+// create date
+const d1 = new Date();
+console.log(d1);
 
-// // converting to number
-// // const result = d1.getTime();
-// // console.log(result);
-
-
+//converting to number
+const re = d1.getTime();
+console.log(re);
 
 
 
 
-// // JS Operators (Assignment, Arithmetic,Comparison,Logical)
-// // x=15;
-// // x++;
-// // x+=15;
-// // console.log(x);
+// Topic 3: JS Operators (Assignment, Arithmetic,Comparison,Logical)
+x=15;
+x++;
+x+=15;
+console.log(x);
+
 // // x--;
 // // x-=15;
 // // x = 20;
@@ -116,52 +115,50 @@
 // //     console.log(false);
 // // }
 
-// // Conditionals 
+//Topic 4: Conditionals 
 // // Test for numbers
-// // var test = prompt("select a number:")
-// // if (test % 2 == 0){
+var test = prompt("select a number:")
+if (test % 2 == 0){
 
-// //         console.log("it is even");
+         console.log("it is even");
     
-// // }
-// // else if (test % 2 == null)
-// // {
+ }
+ else if (test % 2 == null)
+ {
 
-// //     console.log("it is odd");
+ console.log("it is odd");
 
-// // }
-// // else {
+ }
+else {
 
-// //     console.log("it is odd");
+    console.log("it is odd");
+}
 
-// // // to check for even numbers numbers
-// // let number = prompt("Enter a random mumber:");
-// // if (number %2 == 0){
-// //     console.log(" It is an even number");    
-// // }
-// // else{
-// //     console.log(" It is an odd number");
-// // };
-
-
-
+// to check for even numbers 
+let number = prompt("Enter a random mumber:");
+if (number %2 == 0){
+    console.log(" It is an even number");    
+}
+else{
+    console.log(" It is an odd number");
+};
 
 
+// Topic 5: Tenary operators
 
-// // const x= 50;
-// // const y = 100;
-// // let z = (x>y)? "yes": "no";
-// // console.log(z);
+// const x= 50;
+// const y = 100;
+// let z = (x>y)? "yes": "no";
+//console.log(z);
 
-// // const pw = "abcde"
+ // const pw = "abcde"
 // // let user = prompt("Enter your password:")
 // // let signIn = (user === "abcde")? "Welcome": "Incorrect Password"
 // // console.log(signIn);
 
 
 
-
-// // Loops
+// Topic 5:  Loops
 // // For loops(for, for..in,for..of)
 // // while loop (while, do/while)
 // // infinite loop and break
@@ -199,17 +196,7 @@
 //         console.log(i);
 //     }
    
-// }
-
-
-
-
-
-
-
-
-
-
+// 
 
 // // for..in is used to loop through lists,arrays or object
 // //Example 1:To print the adults in a class using a list of their ages
@@ -224,27 +211,10 @@
 // // const trainees= ["Ben", "Tochi","Emma", "Obed", "Excel"];
 // // for (x in trainees){
 // // console.log(trainees[x]);
-// // }
-
-        
-        
-// // for..of is used to loop through lists,arrays or object 
-// //Example 1:       
+// // }     
 
 
-
-// //Example 2:
-// //Example 
-// var x = 34;
-// for
-
-// // infinite for loop(In this case, the break keyword is used to end the loop and avoid memory crash)
-
-// // for(let i = 1; i > 0; i++) {
-// //     // block of code
-// // }
-
-
+//infinite for loop(In this case, the break keyword is used to end the loop and avoid memory crash)
 
 // // var c = 15;
 // // while (c<=15) {
@@ -252,91 +222,15 @@
 // //     console.log("It is just a number");
    
 // // }
+
+//  do..while loop
 // age = 0;
 // do {
 //    console.log(age++); 
 // } while (age<18);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     //  x = 3;
-//     // y = 5;
-//     // w = x+y;
-//     // z = "hello" + x + y;
-
-//     // console.log(w);
-//     // console.log(typeof z);
-
-// // // const z = 15;
-// // // document.write (x+y);
-// // const student ="Andy";
-// // // typeof(student);
-// // const age = 3;
-// // const height = 15.25;
-// // const male = true;
-// // console.log(typeof age);
-// // console.log(age);
-// // console.log(height);
-// // console.log(male);
-
-// // // const school;
-// // let school;
-// // console.log(school);
-// // typeof(age);
+// Topic 6: Arrays and its  methods
 let cars = [ "BMW"," Volkswagen", "Toyota","Honda","IVM"];
 // console.log(cars);
 // let len = cars.pop();
@@ -351,47 +245,20 @@ let o = cars.push('Lexus');
 console.log(cars);
 console.log(cars[0]);
 // let e = cars.splice(2,1,'Volkswagen',"Hyundai");
+
 console.log(cars);
 cars[2] = "Hyundai";
 console.log(cars);
 let arr =[[15,"Volkswagen", true, 15.25],[ "BMW"," Volkswagen", "Toyota","Honda","IVM"]]
 console.log(arr[0][3]);
+// Using arrays and conditionals
 var num = [1,2,3,4,5];
 if (num%2!=0 || num/1) {
     console.log("2 is the third element")
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function
+// Topic 7: function
 function greet() {
     console.log("Hello World!");
 }
@@ -403,49 +270,50 @@ function add(a,b) {
 }
 add(2,4);
 
-// function hello (user) {
-//     alert("you are welcome" +" " + user);
-// }
-// let user = prompt(" Your are:")
-// hello(user);
+function hello (user) {
+    alert("you are welcome" +" " + user);
+ }
+let user = prompt(" Your are:")
+hello(user);
+
 // function that calculates the area of a circle
+// Option A 
 function circle(radius) {
-    this.radius = radius;
-    this.area = function()
-    {
-        return Math.PI * this.radius * this.radius;
-    };
-}
-var c = new circle (3)
+     this.radius = radius;
+     this.area = function()
+     {
+         return Math.PI * this.radius * this.radius;
+     };
+ }
+ var c = new circle (3)
 console.log('area =', c.area().toFixed(2) );
 
-
-fun
-
-
-
-
-
-
-
-
-
-
-
-
+// Option B
 // function circle( radius) {
 
-//     return 2 * 3.14 * radius
+//     return  3.142 * radius ** 2
 // }
+// let radius = 3;
 // let result = circle(radius);
-// console.log( "The area is"+ result);
+
+// console.log( "The area is"+ " " + result);
 
 // function that calculates the area of a square
 function area(s) {
     return s**2;
-}
-let s = parseFloat(prompt("Enter a number for the width"));
-// let t = parseFloat(prompt("Enter a number for the second width"));
-let ans = area(s);
+ }
 
+let s = parseFloat(prompt("Enter a number for the width"));
+let t = parseFloat(prompt("Enter a number for the second width"));
+let ans = area(s);
 console.log( " The answer is"+ ans);
+
+
+// Arrow function
+// Arrow function is more like a shorthand form of functions
+let welcome = () => console.log("Hello World!")
+welcome();
+// arrow functions have limited usage
+const lang = [ "Python", "C++", "JS", "C#", "Go"]
+console.log (lang.map(lang => lang.length));
+
